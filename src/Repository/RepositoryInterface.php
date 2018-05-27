19df5239fc5c8b0c8ab352fde4a4e59867991887
Rougin Royce Gutib <rougingutib@gmail.com>
2018-05-27 13:44:23 +0800

    Update RepositoryInterface

--- src/Repository/RepositoryInterface.php
+++ src/Repository/RepositoryInterface.php
@@ -8,55 +8,6 @@ namespace Rougin\Dexterity\Repository;
  * @package Dexterity
  * @author  Rougin Royce Gutib <rougingutib@gmail.com>
  */
-interface RepositoryInterface
+interface RepositoryInterface extends ReadableInterface, WritableInterface
 {
-    /**
-     * Stores a newly created resource in storage.
-     *
-     * @param  array $data
-     * @return mixed
-     */
-    public function create($data);
-
-    /**
-     * Deletes the specified resource from storage.
-     *
-     * @param  array|integer $id
-     * @return boolean
-     */
-    public function delete($id);
-
-    /**
-     * Finds the specified resource from storage.
-     *
-     * @param  array|integer $id
-     * @return mixed
-     */
-    public function find($id);
-
-    /**
-     * Sets the resource for the repository.
-     *
-     * @param  string $resource
-     * @return self
-     */
-    public function resource($resource);
-
-    /**
-     * Paginates the specified page number and items per page.
-     *
-     * @param  integer $page
-     * @param  integer $limit
-     * @return array
-     */
-    public function paginate($page, $limit);
-
-    /**
-     * Updates the specified resource in storage.
-     *
-     * @param  array|integer $id
-     * @param  array         $data
-     * @return boolean
-     */
-    public function update($id, $data);
 }
