26574441d1119f72952ff87dc01383ce7b196b75
Rougin Gutib <rougingutib@gmail.com>
2019-04-12 15:24:30 +0800

    Update Terminator

---
+++ tests/Fixture/Terminator.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Rougin\Dexterity\Fixture;
+
+/**
+ * Terminator
+ *
+ * @package Dexterity
+ * @author  Rougin Gutib <rougingutib@gmail.com>
+ */
+class Terminator
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Initializes the class.
+     *
+     * @param integer $id
+     * @param string  $name
+     */
+    public function __construct($id, $name = 'Dex')
+    {
+        $this->id = $id;
+
+        $this->name = $name;
+    }
+
+    /**
+     * Returns the ID.
+     *
+     * @return integer
+     */
+    public function id()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the name.
+     *
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+}
